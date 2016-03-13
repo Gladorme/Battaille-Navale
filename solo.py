6a5ba60b28ad39d00ade047725b288b21282eded
MisterSanglad <mistersanglad@gmail.com>
2016-03-13 16:57:16 +0100

    Ajouts des différents fichiers
    Pas de modif au niveau du code

--- solo.py
+++ solo.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar  9 09:32:59 2016
-
-@author: ladorme
-"""
-
 from tkinter import *
 fen = Tk()
 fen.geometry("800x800")
@@ -17,27 +10,27 @@ colonne=10
 #Partie 1:
 can1 = Canvas(fen, width= colonne*d, height= colonne*d)
 can1.pack()
-tuile = []
-val = []
+tuile1 = []
+val1 = []
 for n in range(100):
     x = (n%10)*d
     y = (n//10)*d
-    tuile = tuile + [can1.create_rectangle(x,y,x+d,y+d)]
-    can1.itemconfig(tuile[n], fill="orange")
-    val = val + [can1.create_text(x+d//2, y+d//2)]
-    can1.itemconfig(val[n], text = n)
+    tuile1 = tuile + [can1.create_rectangle(x,y,x+d,y+d)]
+    can1.itemconfig(tuile1[n], fill="orange")
+    val1 = val1 + [can1.create_text(x+d//2, y+d//2)]
+    can1.itemconfig(val1[n], text = n)
 
 #Partie 2:
 can2 = Canvas(fen, width= colonne*d, height= colonne*d)
 can2.pack()
-tuile = []
-val = []
+tuile2 = []
+val2 = []
 for n in range(100):
     x = (n%10)*d
     y = (n//10)*d
-    tuile = tuile + [can2.create_rectangle(x,y,x+d,y+d)]
-    can2.itemconfig(tuile[n], fill="brown")
-    val = val + [can2.create_text(x+d//2, y+d//2)]
-    can2.itemconfig(val[n], text = n)
+    tuile2 = tuile2 + [can2.create_rectangle(x,y,x+d,y+d)]
+    can2.itemconfig(tuile2[n], fill="brown")
+    val2 = val2 + [can2.create_text(x+d//2, y+d//2)]
+    can2.itemconfig(val2[n], text = n)
     
 fen.mainloop()
