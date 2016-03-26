e227ae7428d2141e42fd08aa9f3174247568f5f2
MisterSanglad <mistersanglad@gmail.com>
2016-03-26 21:37:42 +0100

    23/03
    Mise en forme pour le multi fenêtre + bouton Solo pratiquement terminé (Quentin)

--- menu.py
+++ menu.py
@@ -1,38 +1,48 @@
 from tkinter import *
+import solo
+
 fen = Tk()
 fen.geometry("950x534")
 fen.title("Menu - Bataille Navale")
 
+
+
+
 #Definitions
-def solo():
-    info_label.config(text="Informations: \nIl y aura deux tableaux, le premier (celui de gauche)\n vous permettera de placer vos navires et voir où l'adversaire à tirer,\n vos navires ont la couleur blanche et les tirs violets.\n Le second tableau vous permet de voir vos tirs sur l'adversaire,\n si le tirs a touché un bâtiments ennemis la case sera rouge sinon elle sera jaune")
-def multi():
+def Solo():
+    info_label.place(x=500,y=100)
+    info_label.config(text="Informations: \nIl y aura deux tableaux, le premier (celui de gauche)\n vous permettera de placer vos navires et voir où l'adversaire a tiré,\n vos navires ont la couleur blanche et les tirs violets.\n Le second tableau vous permet de voir vos tirs sur l'adversaire,\n si le tirs a touché un vaisseau ennemi la case sera rouge sinon elle sera jaune")
+    bouton_jouer = Button (fen, text="JOUER !", font=("Impact",50), command = solo.jouer(), bg = "#045FB4" )
+    bouton_jouer.place(x=550, y=200)
+	
+def Multi():
+    info_label.place(x=500,y=100)
     info_label.config(text="Multi")
-def leader():
+	
+def Leader():
+    info_label.place(x=500,y=100)
     info_label.config(text="Leaderboard")
 
-
-
+	
 #Fond du menu
 background_img = PhotoImage(file="background.gif")
 can = Canvas(fen, bg="white", width=950, heigh=534,)
 can.place(x=0,y=0)
 can.create_image(473,267, image=background_img)
 
+
 #Elements présents dans le menu
-solo_button = Button(fen, text="Solo", command=solo)
-multi_button = Button(fen, text="Multi", command=multi)
-leaderboard_button = Button(fen, text="Leaderboard", command=leader)
+solo_button = Button(fen, text="Solo", command=Solo, cursor="pirate", width=10)
+multi_button = Button(fen, text="Multijoueurs", command=Multi, cursor="pirate", width=10)
+leaderboard_button = Button(fen, text="Leaderboard", command=Leader, cursor="pirate", width=10)
 info_label = Label(fen, text="")
 
-solo_button.place(x=150,y=100)
-multi_button.place(x=150,y=200)
-leaderboard_button.place(x=130,y=300)
-info_label.place(x=500,y=100)
+solo_button.place(x=70,y=120)
+multi_button.place(x=70,y=220)
+leaderboard_button.place(x=70,y=320)
 
 
 
 
-
-
+#Lancement de la fenêtre
 fen.mainloop()
--- solo.py
+++ solo.py
@@ -1,85 +1,88 @@
 from tkinter import *
-fen = Tk()
-fen.geometry("1400x800")
-fen.title("Solo - Bataille Navale")
 
-#Réglages:
-d=50
-colonne=10
+def jouer():
+    fen = Tk()
+    fen.geometry("1400x800")
+    fen.title("Solo")
+    fen.title("Solo - Bataille Navale")
+        
+    #Réglages:
+    d=50
+    colonne=10
+        
+    #Partie 1:
+    can1 = Canvas(fen, width= colonne*d, height= colonne*d)
+    can1.place(x=100,y=150)
+    tuile1 = []
+    val1 = []
+    for n in range(100):
+        x = (n%10)*d
+        y = (n//10)*d
+        tuile1 = tuile1 + [can1.create_rectangle(x,y,x+d,y+d)]
+        can1.itemconfig(tuile1[n], fill="orange")
+        val1 = val1 + [can1.create_text(x+d//2, y+d//2)]
+        can1.itemconfig(val1[n], text = n)
 
-#Partie 1:
-can1 = Canvas(fen, width= colonne*d, height= colonne*d)
-can1.place(x=100,y=150)
-tuile1 = []
-val1 = []
-for n in range(100):
-    x = (n%10)*d
-    y = (n//10)*d
-    tuile1 = tuile1 + [can1.create_rectangle(x,y,x+d,y+d)]
-    can1.itemconfig(tuile1[n], fill="orange")
-    val1 = val1 + [can1.create_text(x+d//2, y+d//2)]
-    can1.itemconfig(val1[n], text = n)
-##Indication et placement des indications
-label1x = Label(text="  A     B     C     D     E     F    G     H     I      J", font=("Helvetica", 18))
-label1x.place(x=100,y=110)
-label1y1 = Label(text="1",font=("Helvetica", 18))
-label1y2 = Label(text="2",font=("Helvetica", 18))
-label1y3 = Label(text="3",font=("Helvetica", 18))
-label1y4 = Label(text="4",font=("Helvetica", 18))
-label1y5 = Label(text="5",font=("Helvetica", 18))
-label1y6 = Label(text="6",font=("Helvetica", 18))
-label1y7 = Label(text="7",font=("Helvetica", 18))
-label1y8 = Label(text="8",font=("Helvetica", 18))
-label1y9 = Label(text="9",font=("Helvetica", 18))
-label1y10 = Label(text="10",font=("Helvetica", 18))
-label1y1.place(x=70,y=160)
-label1y2.place(x=70,y=210)
-label1y3.place(x=70,y=260)
-label1y4.place(x=70,y=310)
-label1y5.place(x=70,y=360)
-label1y6.place(x=70,y=410)
-label1y7.place(x=70,y=460)
-label1y8.place(x=70,y=510)
-label1y9.place(x=70,y=560)
-label1y10.place(x=60,y=610)
-
-
-#Partie 2:
-can2 = Canvas(fen, width= colonne*d, height= colonne*d)
-can2.place(x=700,y=150)
-tuile2 = []
-val2 = []
-for n in range(100):
-    x = (n%10)*d
-    y = (n//10)*d
-    tuile2 = tuile2 + [can2.create_rectangle(x,y,x+d,y+d)]
-    can2.itemconfig(tuile2[n], fill="brown")
-    val2 = val2 + [can2.create_text(x+d//2, y+d//2)]
-    can2.itemconfig(val2[n], text = n)
-
-##Indication et placement des indications
-label2x = Label(text="  A     B     C     D     E     F    G     H     I      J", font=("Helvetica", 18))
-label2x.place(x=700,y=110) 
-label2y1 = Label(text="1",font=("Helvetica", 18))
-label2y2 = Label(text="2",font=("Helvetica", 18))
-label2y3 = Label(text="3",font=("Helvetica", 18))
-label2y4 = Label(text="4",font=("Helvetica", 18))
-label2y5 = Label(text="5",font=("Helvetica", 18))
-label2y6 = Label(text="6",font=("Helvetica", 18))
-label2y7 = Label(text="7",font=("Helvetica", 18))
-label2y8 = Label(text="8",font=("Helvetica", 18))
-label2y9 = Label(text="9",font=("Helvetica", 18))
-label2y10 = Label(text="10",font=("Helvetica", 18))
-label2y1.place(x=670,y=160)
-label2y2.place(x=670,y=210)
-label2y3.place(x=670,y=260)
-label2y4.place(x=670,y=310)
-label2y5.place(x=670,y=360)
-label2y6.place(x=670,y=410)
-label2y7.place(x=670,y=460)
-label2y8.place(x=670,y=510)
-label2y9.place(x=670,y=560)
-label2y10.place(x=660,y=610)
-
+    ##Indication et placement des indications
+    label1x = Label(fen,text="  A     B     C     D     E     F    G     H     I      J", font=("Helvetica", 18))
+    label1x.place(x=100,y=110)
+    label1y1 = Label(fen,text="1",font=("Helvetica", 18))
+    label1y2 = Label(fen,text="2",font=("Helvetica", 18))
+    label1y3 = Label(fen,text="3",font=("Helvetica", 18))
+    label1y4 = Label(fen,text="4",font=("Helvetica", 18))
+    label1y5 = Label(fen,text="5",font=("Helvetica", 18))
+    label1y6 = Label(fen,text="6",font=("Helvetica", 18))
+    label1y7 = Label(fen,text="7",font=("Helvetica", 18))
+    label1y8 = Label(fen,text="8",font=("Helvetica", 18))
+    label1y9 = Label(fen,text="9",font=("Helvetica", 18))
+    label1y10 = Label(fen,text="10",font=("Helvetica", 18))
+    label1y1.place(x=70,y=160)
+    label1y2.place(x=70,y=210)
+    label1y3.place(x=70,y=260)
+    label1y4.place(x=70,y=310)
+    label1y5.place(x=70,y=360)
+    label1y6.place(x=70,y=410)
+    label1y7.place(x=70,y=460)
+    label1y8.place(x=70,y=510)
+    label1y9.place(x=70,y=560)
+    label1y10.place(x=60,y=610)
+        
+        
+    #Partie 2:
+    can2 = Canvas(fen, width= colonne*d, height= colonne*d)
+    can2.place(x=700,y=150)
+    tuile2 = []
+    val2 = []
+    for n in range(100):
+        x = (n%10)*d
+        y = (n//10)*d
+        tuile2 = tuile2 + [can2.create_rectangle(x,y,x+d,y+d)]
+        can2.itemconfig(tuile2[n], fill="brown")
+        val2 = val2 + [can2.create_text(x+d//2, y+d//2)]
+        can2.itemconfig(val2[n], text = n)
+            
+    ##Indication et placement des indications
+    label2x = Label(fen,text="  A     B     C     D     E     F    G     H     I      J", font=("Helvetica", 18))
+    label2x.place(x=700,y=110) 
+    label2y1 = Label(fen,text="1",font=("Helvetica", 18))
+    label2y2 = Label(fen,text="2",font=("Helvetica", 18))
+    label2y3 = Label(fen,text="3",font=("Helvetica", 18))
+    label2y4 = Label(fen,text="4",font=("Helvetica", 18))
+    label2y5 = Label(fen,text="5",font=("Helvetica", 18))
+    label2y6 = Label(fen,text="6",font=("Helvetica", 18))
+    label2y7 = Label(fen,text="7",font=("Helvetica", 18))
+    label2y8 = Label(fen,text="8",font=("Helvetica", 18))
+    label2y9 = Label(fen,text="9",font=("Helvetica", 18))
+    label2y10 = Label(fen,text="10",font=("Helvetica", 18))
+    label2y1.place(x=670,y=160)
+    label2y2.place(x=670,y=210)
+    label2y3.place(x=670,y=260)
+    label2y4.place(x=670,y=310)
+    label2y5.place(x=670,y=360)
+    label2y6.place(x=670,y=410)
+    label2y7.place(x=670,y=460)
+    label2y8.place(x=670,y=510)
+    label2y9.place(x=670,y=560)
+    label2y10.place(x=660,y=610)
     
-fen.mainloop()
+    fen.mainloop()
